d1e1e7d4837305f6225c2974106e72ee2692022d
Felipe Kazuo <kazuoyatsu@gmail.com>
2019-03-27 22:09:42 -0300

    Add method to sanitize a string

--- textalyze.js
+++ textalyze.js
@@ -1,4 +1,13 @@
 /**
+ * Given an input String, returns true if it's a valid string false otherwise
+ * @param {String} string - The String to be sanitized
+ * @param {Boolean} isString - The String sanitized
+ */
+function validateString(string) {
+  return typeof string !== 'string';
+}
+
+/**
  * Given an input Array, returns a Map containing the count of each item in the input.
  * @param {Array} array - The array of items to count
  * @param {Map} counts - A Map containing the counts of the items in the input array
@@ -20,13 +29,27 @@ function itemCounts(array) {
  * @param {Array} array - The Array containing the characters from the string
  */
 function arrayFrom(string) {
-  if (!string || typeof string !== 'string') {
+  if (validateString(string)) {
     return [];
   }
 
   return string.split('');
 }
-module.exports = { itemCounts, arrayFrom };
+
+/**
+ * Given an input String, returns the string with only lower case characters
+ * @param {String} string - The String to be sanitized
+ * @param {String} sanitizedString - The String sanitized
+ */
+function sanitize(string) {
+  if (validateString(string)) {
+    return '';
+  }
+
+  return string.toLowerCase();
+}
+
+module.exports = { itemCounts, arrayFrom, sanitize };
 
 //
 // running the app
